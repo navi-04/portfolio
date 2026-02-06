c4297257e568fd37a2e0e6f303657ff795b53eb1
navi-04 <naveenrajthiyagarajan6@gmail.com>
2026-02-06 13:51:29 +0530

    intial version

--- ajithkumarvelusamy/main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed('.text', {
-    strings: ['Web Designer', 'Backend developer', 'UI/UX Designer'],
-    typeSpeed: 90,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
