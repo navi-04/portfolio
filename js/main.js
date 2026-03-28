14ccf3c4fd5b9999806e258be39f197ee246ebf9
navi-04 <naveenrajthiyagarajan6@gmail.com>
2026-03-28 13:55:27 +0530

    feat: bind direct mouse tracking to cursor dot

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
   let mouseX = 0, mouseY = 0;
   let ringX = 0, ringY = 0;
 
+  document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    cursorDot.style.transform = `translate(calc(vw / 2 - 50%), calc(vh / 2 - 50%))`;
+    cursorDot.style.left = mouseX + 'px';
+    cursorDot.style.top = mouseY + 'px';
+  });
+
