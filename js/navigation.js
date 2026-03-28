f02648700efd0604c89355cede3c8332329a9ecd
navi-04 <naveenrajthiyagarajan6@gmail.com>
2026-03-28 13:56:38 +0530

    feat: hook accessible mobile navigation hyperlink list

--- js/navigation.js
+++ js/navigation.js
@@ -3,3 +3,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const navToggle = document.querySelector('.nav-toggle');
   const navOverlay = document.querySelector('.nav-overlay');
 
+  const navLinks = document.querySelectorAll('.nav-links li a');
+
